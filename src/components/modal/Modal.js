80ea7445f84e339692a189c917f741cc57a5b776
natalia-a-doskach <natalia.a.doskach@gmail.com>
2022-10-08 18:09:21 +0300

    modal rewritten using createPortal

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,21 +1,22 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { CloseIcon  } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './modal.module.css';
 import ModalOverlay from './ModalOverlay';
+const modalRoot = document.getElementById("react-modals");
 
 function Modal({title,content,close}){
-  return (
-    <>
-    <ModalOverlay close={close}/>
-    <div className={styles.modal}>
-    <CloseIcon type="primary" onClick={close} className={styles.closeBtn}/>
-    <p className={"text text_type_main-default "}>{title}</p>
-    {content}
-    </div>
-    </>
-  )
+    return ReactDOM.createPortal(
+      <>
+      <ModalOverlay close={close}/>
+      <div className={styles.modal}>
+      <CloseIcon type="primary" onClick={close} className={styles.closeBtn}/>
+      <p className={"text text_type_main-default "}>{title}</p>
+      {content}
+      </div>
+      </>,
+      modalRoot
+    );
 }
-
-
 
 export default Modal;
