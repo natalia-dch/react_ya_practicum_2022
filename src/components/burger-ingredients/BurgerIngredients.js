e55fb2b44f14234f839b406262ff70993cadd0bc
natalia-a-doskach <natalia.a.doskach@gmail.com>
2022-10-08 21:26:16 +0300

    BurgerIngredients filtering

--- src/components/burger-ingredients/BurgerIngredients.js
+++ src/components/burger-ingredients/BurgerIngredients.js
@@ -1,35 +1,33 @@
 import React from 'react';
 import styles from './burger-ingredients.module.css';
-import { Tab, CurrencyIcon, Counter } from '@ya.praktikum/react-developer-burger-ui-components';
+import {Tab, CurrencyIcon, Counter} from '@ya.praktikum/react-developer-burger-ui-components';
 import PropTypes from 'prop-types';
 
-function BurgerIngredients({ingredients,onItemClick}){
-  const [current, setCurrent] = React.useState('buns')
-  return (
-    <>
-    <p className={`text text_type_main-medium ml-2 mr-2 `+styles.subheader}>
-     Соберите бургер
-    </p>
+function BurgerIngredients({ingredients, onItemClick}) {
+  const [current, setCurrent] = React.useState('bun')
+  return (<> < p className = {
+    `text text_type_main-medium ml-2 mr-2 ` + styles.subheader
+  } > Соберите бургер < /p>
     <div style={{ display: 'flex'}}>
-      <Tab value="buns" active={current === 'buns'} onClick={setCurrent}>
+      <Tab value="bun" active={current === 'bun'} onClick={setCurrent}>
         Булки
-      </Tab>
-      <Tab value="sauses" active={current === 'sauses'} onClick={setCurrent}>
-        Соусы
-      </Tab>
-      <Tab value="stuffing" active={current === 'stuffing'} onClick={setCurrent}>
-        Начинки
-      </Tab>
-    </div>
-    <div className={styles.ingContainer}>
-    {ingredients.map(
-      (el,ind) => (<Ingredient name={el.name} image={el.image} onClick={()=>{onItemClick(el._id)}} price={el.price} quantity={Math.floor(Math.random()*5)}
-      key={ind} />)
-    )}
-    </div>
-    </>
-
-  )
+      </Tab > <Tab value="sauce" active={current === 'sauce'} onClick={setCurrent}>
+    Соусы
+  </Tab>
+  <Tab value="main" active={current === 'main'} onClick={setCurrent}>
+    Начинки
+  </Tab>
+</div>
+<div className={styles.ingContainer}>
+    {
+    ingredients.map(
+      (el, ind) => el.type === current
+      ? (<Ingredient name={el.name} image={el.image} onClick={() => {
+          onItemClick(el._id)
+        }} price={el.price} quantity={Math.floor(Math.random() * 5)} key={ind}/>)
+      : null)
+  } < /div>
+    </ >)
 }
 
 let ingredientType = PropTypes.shape({
@@ -49,30 +47,25 @@ let ingredientType = PropTypes.shape({
 })
 
 BurgerIngredients.propTypes = {
-  ingredients:PropTypes.arrayOf(ingredientType),
-  onItemClick:PropTypes.func,
+  ingredients: PropTypes.arrayOf(ingredientType),
+  onItemClick: PropTypes.func
 }
 
-function Ingredient({name,image,price,quantity,onClick}){
-  return(
-    <div className={styles.ingredient} onClick={onClick}>
-    {quantity>0 && <Counter count={quantity} size="default" />}
+function Ingredient({name, image, price, quantity, onClick}) {
+  return (<div className={styles.ingredient} onClick={onClick}>
+    {quantity > 0 && <Counter count={quantity} size="default"/>}
     <img src={image} className={styles.pic}/>
-    <p className={"text text_type_digits-default"}>{price}<CurrencyIcon type="primary" /></p>
+    <p className={"text text_type_digits-default"}>{price}<CurrencyIcon type="primary"/></p>
     <p className={"text text_type_main-default"}>{name}</p>
-    </div>
-  )
+  </div>)
 }
-
 
 Ingredient.propTypes = {
-  name:PropTypes.string,
-  image:PropTypes.string,
-  price:PropTypes.number,
-  quantity:PropTypes.number,
-  onClick:PropTypes.func,
+  name: PropTypes.string,
+  image: PropTypes.string,
+  price: PropTypes.number,
+  quantity: PropTypes.number,
+  onClick: PropTypes.func
 }
-
-
 
 export default BurgerIngredients;
