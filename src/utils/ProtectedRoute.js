5cc4a1f3d59bdf5c4511b1a80333f109de73e550
natalia-a-doskach <natalia.a.doskach@gmail.com>
2022-11-13 11:53:59 +0300

    fixes: protected route conditions refactoring

--- src/utils/ProtectedRoute.js
+++ src/utils/ProtectedRoute.js
@@ -13,24 +13,20 @@ export function ProtectedRoute({
     <Route
       {...rest}
       render={({ location }) => {
-        if (isAuth && fromAuthorized) {
+        if (
           //authorized users  are redirected to home page
-          return <Redirect to={{ pathname: "/", state: { from: location } }} />;
-        } else if (
-          !isAuth &&
-          rest.path === "/reset-password" &&
-          location?.state?.from !== "/forgot-password"
-        ) {
+          (isAuth && fromAuthorized) ||
           //unauthorized user can go to to reset password from "/forgot-password"
+          (!isAuth &&
+            rest.path === "/reset-password" &&
+            location?.state?.from !== "/forgot-password")
+        ) {
           return <Redirect to={{ pathname: "/", state: { from: location } }} />;
         } else if (!isAuth && fromUnauthorized) {
           //unauthorized users are redirected to login
           return (
             <Redirect to={{ pathname: "/login", state: { from: location } }} />
           );
-        } else if (isAuth && fromAuthorized) {
-          //authorized users  are redirected to home page
-          return <Redirect to={{ pathname: "/", state: { from: location } }} />;
         } else {
           return children;
         }
